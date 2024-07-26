3aa04d22bd0be28de1720e451509742877210a13
Jed Lankitus <jlankitus@gmail.com>
2024-07-26 00:20:16 -0700

    Created fancy way of  'drawing' each pattern into the scriptable object
    inspired by this: https://www.youtube.com/watch?v=7QjsE9N2kjI&ab_channel=PwnDevs

--- GameOfLife3D/Assets/Scripts/LifePattern.cs
+++ GameOfLife3D/Assets/Scripts/LifePattern.cs
@@ -5,6 +5,12 @@ public class LifePattern : ScriptableObject
 {
     public string patternName;
     public GameObject patternPrefab;
+    public int width = 8;
+    public int height = 8;
+    public int[] patternGrid; // Flattened grid
+    public Vector2Int startingPosition; // Specify the starting position
+
+    public bool randomize;
 
     // Cool trick to default the patternName to the object name.
     private void OnValidate()
@@ -12,6 +18,33 @@ public class LifePattern : ScriptableObject
         if (string.IsNullOrEmpty(patternName))
         {
             patternName = name;
+        }
+
+        if (patternGrid == null || patternGrid.Length != width * height)
+        {
+            patternGrid = new int[width * height];
+        }
+    }
+
+    public int[,] GetPatternGrid2D()
+    {
+        int[,] grid2D = new int[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                grid2D[x, y] = patternGrid[y * width + x];
+            }
+        }
+        return grid2D;
+    }
+
+    public void RandomizeGrid()
+    {
+        System.Random rnd = new System.Random();
+        for (int i = 0; i < patternGrid.Length; i++)
+        {
+            patternGrid[i] = rnd.Next(2);
         }
     }
 }
---
+++ GameOfLife3D/Assets/Scripts/LifePatternEditor.cs
@@ -0,0 +1,41 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LifePattern))]
+public class LifePatternEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LifePattern lifePattern = (LifePattern)target;
+
+        lifePattern.patternName = EditorGUILayout.TextField("Pattern Name", lifePattern.patternName);
+        lifePattern.startingPosition = EditorGUILayout.Vector2IntField("Starting Position", lifePattern.startingPosition);
+        lifePattern.width = EditorGUILayout.IntField("Width", lifePattern.width);
+        lifePattern.height = EditorGUILayout.IntField("Height", lifePattern.height);
+
+        if (lifePattern.patternGrid == null || lifePattern.patternGrid.Length != lifePattern.width * lifePattern.height)
+        {
+            lifePattern.patternGrid = new int[lifePattern.width * lifePattern.height];
+        }
+
+        for (int y = 0; y < lifePattern.height; y++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (int x = 0; x < lifePattern.width; x++)
+            {
+                lifePattern.patternGrid[y * lifePattern.width + x] = EditorGUILayout.IntField(lifePattern.patternGrid[y * lifePattern.width + x], GUILayout.Width(20));
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button("Randomize Grid"))
+        {
+            lifePattern.RandomizeGrid();
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(lifePattern);
+        }
+    }
+}
